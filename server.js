2182802936b27a89654015108bbbcb1b890becbe
user <ygit config --global user.emaimowandugu@gmail.comgit config --global user.email>
2021-05-06 12:19:53 +0300

    add invalid date endpoint api

--- server.js
+++ server.js
@@ -22,10 +22,15 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
+  console.log({greeting: 'hello API'});
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+  console.log(dateString);
+  res.json({"error": "Invalid Date"});
+});
 
 // listen for requests :)
 var listener = app.listen(port, function () {
